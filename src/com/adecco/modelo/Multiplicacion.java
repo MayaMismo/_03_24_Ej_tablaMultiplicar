bffaa228ca5e308de102028aeaa72dc2b305ac5f
MayaMismo <mjrodriher@gmail.com>
2023-03-24 13:29:34 +0100

    Version 13:28

--- src/com/adecco/modelo/Multiplicacion.java
+++ src/com/adecco/modelo/Multiplicacion.java
@@ -1,2 +1,31 @@
-package com.adecco.modelo;public class Multiplicacion {
+package com.adecco.modelo;
+
+public class Multiplicacion {
+    //Construir una aplicación que imprima por pantalla las tablas de multiplicar. Debe
+    //contar con un constructor que indica la tabla que se va a mostrar. Debe contar con un
+    //método que dé como resultado un string para mostrar diez líneas de la tabla del
+    //número dado en el constructor
+
+    //1.ATRIBUTOS
+    private int numero; //aquí numero es ATRIBUTO pq está dentro de una clase
+
+    //2. METODO
+
+    public String calcularTabla (){
+        String resultado = "La tabla de multiplicar del " +numero +"\n"; //el \n es para hacer salto de línea
+        //tenemos que meter una conficion FOR - Aquí resultado se me convirtió en VARIABLE pq está dentro
+        //de un método
+        for (int i=1; i<=10; i++){
+            //ahora debo concatenar:
+            resultado = resultado +i + "x" +numero + " = " +(i*numero) + "\n";
+            //esta línea la podría resumir con:
+            //resultado += i + "x" +numero + " = " +(i*numero) + "\n";
+        }
+        return resultado;
+    }
+    //3.Constructor (siempre con el nombre de la clase)
+    public Multiplicacion (int numero){
+        //ahora le tengo que dar valor a mi atributo:
+        this.numero = numero;
+    }
 }
---
+++ src/com/adecco/presentacion/ProbarMultiplicacion.java
@@ -0,0 +1,11 @@
+package com.adecco.presentacion;
+
+import com.adecco.modelo.Multiplicacion;
+
+public class ProbarMultiplicacion {
+    public static void main(String[] args) { //este es el metodo principal
+        Multiplicacion m1 = new Multiplicacion(6);
+         System.out.println(m1.calcularTabla());
+
+            }
+}
